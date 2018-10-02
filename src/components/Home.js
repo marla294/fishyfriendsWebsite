2fd8b1ba51ea3cececfd5a0914bd5e4559763da6
Marla Foreman <marla294@gmail.com>
2018-10-02 15:58:58 -0500

    Printing out the fish names to the screen
    Just basically remembering how to React/JavaScript

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,23 @@
 import React from "react";
 
 class Home extends React.Component {
-	state = { fish: null };
+	state = { fish: [] };
 
 	componentDidMount() {
 		fetch("http://127.0.0.1:8080/api/fish").then(res => {
-			res.json().then(r => console.log(r));
+			res.json().then(r => this.setState({ fish: r }));
 		});
 	}
 
+	renderFish = () => {
+		const names = this.state.fish.map(fish => fish.Name);
+
+		return names.map(name => <div>{name}</div>);
+	};
+
 	render() {
-		return <div>{this.state.fish}</div>;
+		return <div>{this.renderFish()}</div>;
 	}
 }
 
 export default Home;
-
