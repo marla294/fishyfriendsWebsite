036381822f6292a0e27f6d9b9e8851efdfc0b443
Marla Foreman <marla294@gmail.com>
2018-10-03 10:34:37 -0500

    Installing eslint on this project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import Router from "./components/Router";
+import "./css/Styles.css";
 
 render(<Router />, document.getElementById("root"));
