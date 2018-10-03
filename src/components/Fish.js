6487200a8d5325be8c175d6a7021d175f1a11445
Marla Foreman <marla294@gmail.com>
2018-10-03 14:19:02 -0500

    Adding styles to Fish component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,9 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../css/Fish.css";
 
 class Fish extends React.Component {
 	render() {
-		return <div>{this.props.name}</div>;
+		return (
+			<div className="fish">
+				<img src="/images/fish/clown.png" alt="fishPic"/>
+				<div className="name">{this.props.name}</div>
+			</div>
+		);
 	}
 }
 
