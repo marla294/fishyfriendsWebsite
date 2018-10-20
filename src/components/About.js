5b791136d020e9f6e093f9d76edfca9d02045aba
Marla Foreman <marla294@gmail.com>
2018-10-19 21:13:57 -0500

    Starting to write the About page information

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,17 @@ import "../css/About.css";
 
 class About extends React.Component {
 	render() {
-		return (<div>About component</div>);
+		return (
+			<div className="About">
+				<h1>How This Works</h1>
+				<h3>Explanation</h3>
+				<p>One of the challenging things about setting up a saltwater aquarium is stocking it with fish.  Not all aquarium fish get along.  Some fish are docile while others are aggressive, even with members of their own species.  If fish don't get along, they can harm and even kill other fish.</p>
+
+				<p>This tool will help you plan and select fish that will be friends.  Select one fish and the others will automatically color in according to how they get along with the selected fish.  A fish colored green will pair well with the selected fish, while a yellow fish may work under some circumstances with the selected fish.  A red fish means it will not work with your selected fish.</p>
+
+				<p>You can select more than one fish.  In the case of multiple fish being selected, the other fish will color in according to their worst compatibility match.  You can see what the individual compatibility with the selected fishes are by clicking the "i" button on any fish.</p>
+			</div>
+		);
 	}
 }
 
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
+import About from "./About";
 
 const Router = props => (
 	<BrowserRouter>
 		<Switch>
 			<Route exact path="/" component={Home} />
 			<Route path="/Home" component={Home} />
+			<Route path="/About" component={About} />
 		</Switch>
 	</BrowserRouter>
 );
