c8e83a30a0357b1bc8831045bf70d394ff5259a9
Marla Foreman <marla294@gmail.com>
2018-10-21 10:41:03 -0500

    Adding link to About page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,18 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div className="home">
+				<a onClick={this.goToAbout} className="homeGoToAbout">Help Using This Page</a>
 				<h1 className="homeTitle">Fish Friends</h1>
 				<h3 className="homeSubTitle"><i>Select up to ten fish and see how they get along with the others!</i></h3>
 				<FishTank/>
 			</div>
 		);
 	}
+
+	goToAbout = event => {
+		event.preventDefault();
+		this.props.history.push("/About");
+	};
 }
 
 export default Home;
