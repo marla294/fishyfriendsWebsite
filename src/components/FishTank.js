b424a2fbde94c3822024da6b66d3b2d1f6546cf6
Marla Foreman <marla294@gmail.com>
2018-10-23 14:02:22 -0500

    Making fishy-friends.com live!
    Changing API url over to production

--- src/components/FishTank.js
+++ src/components/FishTank.js
@@ -5,7 +5,8 @@ import "../css/FishTank.css";
 
 class FishTank extends React.Component {
 	state = { selectedFishes: [], compatibility: [], selectedFish: null };
-	url = "http://127.0.0.1:8080/api/";
+	url = "https://www.axequest.com/fishfriends/api/";
+
 	maxSelected = 10;
 
 	render() {
