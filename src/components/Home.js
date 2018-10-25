90f1984050b3bff3f177e91aaedd9eb8d09df32d
Marla Foreman <marla294@gmail.com>
2018-10-25 11:13:42 -0500

    Using Link instead of anchor tag for "go to About" on Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,19 @@
 import React from "react";
 import FishTank from "./FishTank";
+import { Link } from 'react-router-dom';
 import "../css/Home.css";
 
 class Home extends React.Component {
 	render() {
 		return (
 			<div className="home">
-				<a onClick={this.goToAbout} className="homeGoToAbout">Help Using This Page</a>
+				<Link to="/About" className="homeGoToAbout">Help Using This Page</Link>
 				<h1 className="homeTitle">Fish Friends</h1>
 				<h3 className="homeSubTitle"><i>Select up to ten fish and see how they get along with the others!</i></h3>
 				<FishTank/>
 			</div>
 		);
 	}
-
-	goToAbout = event => {
-		event.preventDefault();
-		this.props.history.push("/About");
-	};
 }
 
 export default Home;
